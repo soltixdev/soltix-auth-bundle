09ed659c824025e736346cca67b97d3336da0eb2
lukasz lma <lukasz.m@zoom-marketing.pl>
2022-12-23 12:48:31 +0100

    auth

--- src/EventSubscriber/SoltixAuthSubscriber.php
+++ src/EventSubscriber/SoltixAuthSubscriber.php
@@ -19,8 +19,27 @@ class SoltixAuthSubscriber implements EventSubscriberInterface
 
     public function onKernelRequest(RequestEvent $event)
     {
+        if ($event->getRequest()->headers->has('Authorization') === false) {
+            return;
+        }
 
-
+        if ($event->getRequest()->headers->has('PHP_AUTH_USER') === false) {
+            header('WWW-Authenticate: Basic realm="My Realm"');
+            header('HTTP/1.0 401 Unauthorized');
+            echo 'Text to send if user hits Cancel button';
+            exit;
+        } else {
+            if (
+                $event->getRequest()->headers->get('PHP_AUTH_USER') !== 'lma' &&
+                $event->getRequest()->headers->get('PHP_AUTH_PW') !== 'lma'
+            )
+            {
+                header('WWW-Authenticate: Basic realm="My Realm"');
+                header('HTTP/1.0 401 Unauthorized');
+                echo 'Text to send if user hits Cancel button';
+                exit;
+            }
+        }
     }//end onKernelRequest()
 
     public static function getSubscribedEvents()
