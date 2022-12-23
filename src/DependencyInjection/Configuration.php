c487f3e1f160863762bcffd63b6c6f6a1d55e0f6
lukasz lma <lukasz.m@zoom-marketing.pl>
2022-12-23 10:56:57 +0100

    Add Configuration

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Configuration.
+ *
+ * @author  SOLTIX <lukasz.m@soltix.pl>
+ * @license <https://opensource.org/licenses/MIT> MIT
+ */
+declare(strict_types=1);
+
+
+namespace Soltix\Bundle\AuthBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder('soltix-auth');
+        dd($treeBuilder);
+    }
+}
