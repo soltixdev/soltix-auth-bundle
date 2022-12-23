a6aadc70c49189c318e63cb4ce2587b1af6aa222
lukasz lma <lukasz.m@zoom-marketing.pl>
2022-12-23 12:36:47 +0100

    .

--- src/DependencyInjection/SoltixAuthExtension.php
+++ src/DependencyInjection/SoltixAuthExtension.php
@@ -10,15 +10,20 @@ declare(strict_types=1);
 
 namespace Soltix\Bundle\AuthBundle\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class SoltixAuthExtension extends Extension
 {
 
     public function load(array $configs, ContainerBuilder $container)
     {
-        dd(1);
-        // TODO: Implement load() method.
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../../config')
+        );
+        $loader->load('services.yaml');
     }
 }
