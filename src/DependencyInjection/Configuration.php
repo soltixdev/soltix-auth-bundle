3c8cb769fdafa677a7abf08570c0a8e9d4524927
lukasz lma <lukasz.m@zoom-marketing.pl>
2022-12-23 12:58:06 +0100

    configuration

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Configuration.
+ *
+ * @author  SOLTIX <lukasz.m@soltix.pl>
+ * @license <https://opensource.org/licenses/MIT> MIT
+ */
+declare(strict_types=1);
+
+
+namespace Soltix\Bundle\AuthBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder('soltix_auth');
+
+        $treeBuilder->getRootNode()
+            ->children()
+                ->arrayNode('basic_auth')
+                    ->children()
+                        ->scalarNode('realm')->end()
+                        ->scalarNode('users')->end()
+                    ->end()
+                ->end() // basic_auth
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
--- src/DependencyInjection/SoltixAuthExtension.php
+++ src/DependencyInjection/SoltixAuthExtension.php
@@ -20,6 +20,10 @@ class SoltixAuthExtension extends Extension
 
     public function load(array $configs, ContainerBuilder $container)
     {
+        $configuration =  new Configuration();
+
+        $config = $this->processConfiguration($configuration, $configs);
+
         $loader = new YamlFileLoader(
             $container,
             new FileLocator(__DIR__.'/../../config')
